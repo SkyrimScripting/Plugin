887b96b22d66c6a6645d900ba81874f6630e4b3a
Mrowr Purr <87039589+mrowrpurr@users.noreply.github.com>
2022-12-28 00:02:52 -0800

    TODO support for UI/BSInputDeviceManager events

--- include/SkyrimScripting/Plugin/EventCallback.h
+++ include/SkyrimScripting/Plugin/EventCallback.h
@@ -2,6 +2,8 @@
 
 #include <SkyrimScripting/Plugin/CallbackEventSink.h>
 
+// TODO: support UI and BSInputDeviceManager events (and others, if any)
+
 namespace SkyrimScripting::Plugin::EventCallback {
     template <typename EventType>
     void On(std::function<void(const EventType*)> callback) {
